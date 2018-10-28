9559440c472bbdcfdd7507160f23f2dcd5f204d2
Owais Ali <owaisali.cs@gmail.com>
2018-10-29 03:33:35 +0500

    add project header-parser

--- server.js
+++ server.js
@@ -24,9 +24,17 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/whoami",function(req,res) {
+	res.json({"ipaddress": req.ip,
+	"language": req.headers['accept-language'],
+	"software": req.headers['user-agent'], 
+	//"test":req.headers
+	});
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
